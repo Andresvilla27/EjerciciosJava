fbb3c873f6363230b53ff97f3e379162a91d8ce5
Andresvilla27 <motorolasmartflip@gmail.com>
2022-09-30 00:01:22 -0300

    Ejercicio de Tema 3

---
+++ Coche.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Coche {
+    public static void main(String[] args) {
+        int puertas;
+
+        puertas = 2;
+
+        Object miCoche;
+
+        miCoche = puertas + 1 ;
+
+        System.out.println("La cantidad de puertas es: "+miCoche);
+    }
+}
---
+++ Tema3.java
@@ -0,0 +1,18 @@
+public class Tema3 {
+    public static void main(String[] args) {
+        int param1 = 10;
+        int param2 = 20;
+        int param3 = 30;
+        int suma;
+        
+        suma = (param1+param2+param3);
+
+        System.out.println("La suma de los tres parametros es: "+suma);
+    }
+
+    
+}
+
+
+
+    
